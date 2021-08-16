9eef28156849f1862444d95aa052eac64318e163
EveOfMaly <john.a.kyeremeh@gmail.com>
2021-08-16 06:52:47 -0700

    commit tree fd01d8b5cfe1e14073e07f32f30e46849c51ac09

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,11 @@
 # code your #position_taken? method here!
+#define a method that checks if a given space on the board is empty i.e equal to " "
+#Address the "edge cases" - scenarios that qualify as an empty space that isn't equal to the literal " ". For example we would wnat to consider "" to be an empty space as well.
+
+def position_taken?(board, index)
+    if board[index] == " " || board[index] == "" || board[index] == nil
+        false
+    elsif board[index] == "X" or board[index] == "O"
+         true 
+    end
+end
